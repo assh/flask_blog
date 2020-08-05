616bca8dcfb64ff9667f5d5bf216ba241d67af94
Asish <asish.panda13@gmail.com>
2020-08-05 19:36:47 +0400

    Added login and register page

--- blog.py
+++ blog.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import *
+import os
 app = Flask(__name__)
 
-
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 post = [
     {
         'author': 'Test 1',
@@ -21,12 +23,33 @@ post = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html',posts=post)
+    return render_template('home.html', posts=post)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html',title = 'About')
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@test.com' and form.password.data == 'password':
+            flash('You have logged in.', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Incorrect Username or Password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
